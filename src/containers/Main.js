df957977d387656bbe31f1e2faa3c52c2708a615
James Swatton <jamessswatton@gmail.com>
2019-07-05 16:49:30 +0100

    add fetch method Main Container - need to refactor table in state

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,10 +10,11 @@ class Main extends Component {
             tables: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             customers: [{ name: "Fred", phone: "07800900900", counter: 0 }],
             bookings: [{ date: "01/01/2019", time: "18:00", party_size: 4 }],
-            urls: [{customersURL:'http://localhost:8080/customers'}, {bookingsURL: 'http://localhost:8080/bookings'}]
+            urls: [{customersURL:'http://localhost:8080/customers'}, {bookingsURL: 'http://localhost:8080/bookings'}, {diningTablesURL: 'http://localhost:8080/diningTables'}]
         }
         this.makeBooking = this.makeBooking.bind(this);
         this.postDetails = this.postDetails.bind(this);
+        this.fetchDetails = this.fetchDetails.bind(this);
     }
 
     makeBooking(booking) {
@@ -41,12 +42,20 @@ class Main extends Component {
       }))
     }
 
+    fetchDetails(url, stateKey) {
+      fetch(url)
+          .then(res => res.json())
+          .then(customerData => this.setState({[`${stateKey}`]: customerData._embedded[`${stateKey}`]}
+          ))
+    }
+
 
     componentDidMount(){
         fetch('http://localhost:8080/customers')
             .then(res => res.json())
             .then(customerData => this.setState({customers: customerData._embedded.customers}
             ))
+        this.fetchDetails(this.state.urls[2].diningTablesURL, "diningTables")
     }
 
     render() {
