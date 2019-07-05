8e8d834960ddb3e7c064afc40e22619e988e0f43
James Swatton <jamessswatton@gmail.com>
2019-07-05 16:23:28 +0100

    added Customer details can be posted

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -9,18 +9,38 @@ class Main extends Component {
         this.state = {
             tables: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             customers: [{ name: "Fred", phone: "07800900900", counter: 0 }],
-            bookings: [{ date: "01/01/2019", time: "18:00", party_size: 4 }]
+            bookings: [{ date: "01/01/2019", time: "18:00", party_size: 4 }],
+            urls: [{customersURL:'http://localhost:8080/customers'}, {bookingsURL: 'http://localhost:8080/bookings'}]
         }
         this.makeBooking = this.makeBooking.bind(this);
+        this.postDetails = this.postDetails.bind(this);
     }
 
     makeBooking(booking) {
         const custDetails = { name: booking.name, phone_number: booking.phone_number }
         const bookDetails = { date: booking.date, time: booking.time, party_size: booking.size }
-        this.setState((prevState) => {
-            return prevState.bookings.push(bookDetails), prevState.customers.push(custDetails);
-        })
+        // this.setState((prevState) => {
+        //     return prevState.bookings.push(bookDetails), prevState.customers.push(custDetails);
+        // })
+        this.postDetails(this.state.urls[0].customersURL, custDetails, "customers")
+        // this.postDetails(this.state.urls[1].bookingsURL, bookDetails, "bookings")
     }
+
+    postDetails(url, body, stateKey ){
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+      .then(res => res.json())
+      .then(returnData => this.setState(prevState => {
+        return {[`${stateKey}`]: prevState[`${stateKey}`].concat(returnData)}
+      }))
+    }
+
 
     componentDidMount(){
         fetch('http://localhost:8080/customers')
