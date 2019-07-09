0b6c6270bb89beed796c72086e5d191da90ac2b5
Joseph Young <jsyoung0891@gmail.com>
2019-07-09 11:10:57 +0100

    added the draggable function but its not happy with it yet

--- src/components/FloorPlan/FloorPlan.js
+++ src/components/FloorPlan/FloorPlan.js
@@ -4,13 +4,27 @@ import TableDetail from './TableDetail';
 
 const FloorPlan = (props) => {
     const tables = props.state.diningTables.map((number, index) => {
-        return <div className="table" key={index} >{number.tableName} <TableDetail bookingDetail={props.state.customerDetail}></TableDetail></div>
+        return <div className="table" draggable="true" onDragStart="drag(event)" id="diningTable" key={index} >{number.tableName} <TableDetail bookingDetail={props.state.customerDetail}></TableDetail></div>
     })
+
+    function allowDrop(event){
+        event.preventDefault();
+    }
+
+    function drag (event){
+        event.dataTransfer.setData("diningTable", event.target.id);
+    }
+
+    function drop (event){
+        event.preventDefault();
+        const data = event.dataTransfer.getData("diningTable");
+        event.target.appendChild(document.getElementById(data));
+    }
 
     return (
         <Fragment>
             <div className="floorplan-container">
-                <div className="floorplan">
+                <div className="floorplan" onDrop="drop(event)" onDragOver="allowDrop(event)">
                     {tables}
                  </div>
             </div>
