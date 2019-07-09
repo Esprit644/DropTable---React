660ef8c64164f6c61b3ad810a4666ab546c91b47
Joseph Young <jsyoung0891@gmail.com>
2019-07-09 17:52:04 +0100

    wrote a lovely function for making divs. might not work but its nice

--- src/components/BookingForecast.js
+++ src/components/BookingForecast.js
@@ -18,9 +18,42 @@ const BookingForecast = (props) => {
         times.push(index +":00");
     }
 
-    // function fillTimeSlot(){
+    // function takes in the array of bookings
+    // for each booking
+    // take out the time and table number
+    // add those number to the css
+    // return a div with the css attached
+    // send those divs down to the forecast
 
-    // }
+    function fillTimeSlot(arrayOfBookings){
+        bookedTables = []
+        for(booking of arrayOfBookings){
+            const timeStart = booking.time
+            const tableNumber = booking.diningTable.id
+            const timeStartAdjusted = timeStart + 4
+            const tableNumberAdjusted = tableNumber + 2
+
+            const booked = {
+                gridColumn: ' span 8 /' + timeStartAdjusted,
+                gridRow: 'span 1 /' + tableNumberAdjusted,
+                backgroundColor: '#4cd4a0'
+               }
+
+              bookedTables.push(<div style={booked}></div>) 
+
+        }
+    }
+    
+        const timeStart = 21;
+        const tableNumber = 3;
+
+       
+       
+
+        
+        
+    
+
 
     const displayTimes = times.map((time, index) => {
         return <div key={index} className="time">{time}</div>
@@ -32,15 +65,15 @@ const BookingForecast = (props) => {
         gridTemplateRows: "3vh repeat(" + numberOfTables + ", 1fr)"
     };
 
-
     return (
         <Fragment>
             <div className="view">
                 <div className="tableNames">
                     {tableNumbers}
                 </div>
-                <div className="forecast" style={forecast}>
+                <div className="forecast" style={forecast} >
                     {displayTimes}
+                    {bookedTables}
                 </div>
             </div>
         </Fragment>
