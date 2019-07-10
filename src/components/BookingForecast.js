e7be9e495c9a4b6e984909d8aed8116f88b1dee7
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-07-10 16:51:56 +0100

    dead code removal

--- src/components/BookingForecast.js
+++ src/components/BookingForecast.js
@@ -24,69 +24,6 @@ const BookingForecast = props => {
     times.push(index + ":00");
   }
 
-  const testArray = [
-    {
-      time: "12:00",
-      diningTable: {
-        id: 1
-      }
-    },
-    {
-      time: "21:00",
-      diningTable: {
-        id: 2
-      }
-    },
-    {
-      time: "17:00",
-      diningTable: {
-        id: 2
-      }
-    }
-  ];
-
-//   function createHandleBookingClick(divValue) {
-//     return () => {
-//       console.log(divValue)
-//       props.updateSelectedBooking(divValue)
-//     }
-//   }
-
-//   function fillTimeSlots(props) {
-//     const bookedTables = [];
-//     for (const booking of props.bookings) {
-//       const timeStart = booking.time;
-//       const timeWithoutDashes = timeStart.replace(":", "");
-//       const timeStartToInteger = parseInt(timeWithoutDashes);
-//       const timeStartAdjusted = (timeStartToInteger - 1200) / 25 + 9;
-
-//       const tableName = booking.diningTable.tableName;
-//       const tableJustTheNumber = tableName.replace("Table", "");
-//       const tableNumber = parseInt(tableJustTheNumber);
-//       const tableNumberAdjusted = tableNumber + 2;
-//       const customerName = booking.customer.name
-
-//       const divValue = {
-//         tableId: tableJustTheNumber,
-//         time: timeStart
-//       }
-
-//       const booked = {
-//         gridColumn: " span 8 /" + timeStartAdjusted,
-//         gridRow: "span 1 /" + tableNumberAdjusted,
-//         backgroundColor: "#4cd4a0",
-//         hover: {
-//           backgroundColor: "#333"
-//         }
-//       };
-
-//       const handleBookingClick = createHandleBookingClick(divValue);
-
-//       bookedTables.push(<button style={booked} onClick={handleBookingClick}>{customerName}</button>);
-//     }
-//     return bookedTables;
-//   }
-
   const displayTimes = times.map((time, index) => {
     return (
       <div key={index} className="time">
